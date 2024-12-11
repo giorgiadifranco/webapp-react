8765c41f6b1302366454d13b373c23340def888a
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-11 14:47:25 +0100

    add header component

---
+++ react-front/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom"
+
+export default function Header(){
+
+    return(
+
+        <header>
+            <nav>
+            <NavLink to='/'>Home</NavLink>
+            </nav>
+        </header>
+
+
+    )
+}
