5f3d25ffb148c78b86bc53a9076c2b790819f661
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-12 11:26:28 +0100

    import use effect and useState

--- react-front/src/pages/MoviesHome.jsx
+++ react-front/src/pages/MoviesHome.jsx
@@ -1,39 +1,10 @@
 import MovieCard from '../components/MovieCard'
+import { useState, useEffect} from 'react'
 
 export default function MoviesHome(){
 
-    const movies = [
-        {
-          id: 1,
-          name: "Inception",         
-          text: "Un capolavoro di Christopher Nolan che esplora il mondo dei sogni."
-          
-        },
-        {
-          id: 2,
-          name: "The Matrix",        
-          text: "Un film rivoluzionario che combina azione e filosofia in una realtà simulata."
-         
-        },
-        {
-          id: 3,          
-          name: "Interstellar",          
-          text: "Un'avventura spaziale emozionante che esplora l'amore e la sopravvivenza."
-          
-        },
-        {
-          id: 4,
-          name: "The Dark Knight",
-          text: "Un'epica lotta tra Batman e Joker in una Gotham oscura e realistica.",
-          
-        },
-        {
-          id: 5,         
-          name: "Pulp Fiction",
-          text: "Un cult di Quentin Tarantino pieno di dialoghi brillanti e scene iconiche."       
-        },
-      ];
-      
+
+
 
     return(
         <>  
@@ -43,9 +14,9 @@ export default function MoviesHome(){
                 <div className='container'>
                     <h2 className='p-5'>Best Rating Movies</h2>
                     <div className='row row-cols-3 p-5 g-5'>
-                     {
+                     {/*
                         movies.map(movie =>(<div className='col'key={movie.id}><MovieCard movie={movie}/> </div>) )
-                     }
+                     */}
                     </div>  
                 </div>
                 
