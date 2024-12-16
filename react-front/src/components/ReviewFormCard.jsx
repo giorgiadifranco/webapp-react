1eddb2c2db8d8f291a63f376f3813dc787314bb0
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-16 15:24:01 +0100

    add input

--- react-front/src/components/ReviewFormCard.jsx
+++ react-front/src/components/ReviewFormCard.jsx
@@ -1,6 +1,15 @@
-export default function ReviewFormCard( ){
 
-    function handleFormSubmit(){
+import { useState } from "react"
+
+export default function ReviewFormCard(movie_id, success, handleSuccess){
+
+    const [username, setUsername] = useState('')
+    const [rating, setRating] = useState('')
+    const [review, setReview]= useState(0)
+
+    function handleFormSubmit(e){
+
+         
 
         e.preventDefault()
         console.log('here');
@@ -17,7 +26,10 @@ export default function ReviewFormCard( ){
                 <div className="card">
                     <div className="card-body">
 
-                        <form onSubmit={handleFormSubmit}></form>
+                        <form onSubmit={handleFormSubmit}>
+                            <iput name="username" type="text" className="form-control" placeholder="name" value={username} onChange={(e)=> setUsername(e.target.username)} />
+
+                        </form>
                     </div>
                 </div>
             </div>
