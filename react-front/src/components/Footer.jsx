aceb49336121eaca1594681f25355d6ca714e21f
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-17 15:54:16 +0100

    fix footer

--- react-front/src/components/Footer.jsx
+++ react-front/src/components/Footer.jsx
@@ -8,34 +8,34 @@ export default function Footer(){
         <footer className="bg-dark text-light py-3">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6 col-lg-3 mb-3">
-                        <Link className='text-decoration-none text-white link-warning'><h5>About us</h5></Link>
-                 
-                        <ul className="list-unstyled">
-                            <li></li>
-                        </ul> 
-                    </div>
-                    <div className="col-md-6 col-lg-3 mb-3">
-                    <Link className='text-decoration-none text-white link-warning'><h5>Services</h5></Link>
-                 
-                        <ul className="list-unstyled">
-                            <li></li>
-                        </ul> 
-                    </div>
-                    <div className="col-md-6 col-lg-3 mb-3">
-                    <Link className='text-decoration-none text-white link-warning'><h5>FAQ</h5></Link>
-                 
-                        <ul className="list-unstyled">
-                            <li></li>
-                        </ul> 
-                    </div>
-                    <div className="col-md-6 col-lg-3 mb-3">
-                    <Link className='text-decoration-none text-white link-warning'><h5>Privacy Policy</h5></Link>
-                 
-                        <ul className="list-unstyled">
-                            <li></li>
-                        </ul> 
-                    </div>
+                        <div className="col-md-6 col-lg-3 mb-3">
+                            <Link className='text-decoration-none text-white link-warning'><h5>About us</h5></Link>
+                    
+                            <ul className="list-unstyled">
+                                <li></li>
+                            </ul> 
+                        </div>
+                        <div className="col-md-6 col-lg-3 mb-3">
+                        <Link className='text-decoration-none text-white link-warning'><h5>Services</h5></Link>
+                    
+                            <ul className="list-unstyled">
+                                <li></li>
+                            </ul> 
+                        </div>
+                        <div className="col-md-6 col-lg-3 mb-3">
+                        <Link className='text-decoration-none text-white link-warning'><h5>FAQ</h5></Link>
+                    
+                            <ul className="list-unstyled">
+                                <li></li>
+                            </ul> 
+                        </div>
+                        <div className="col-md-6 col-lg-3 mb-3">
+                        <Link className='text-decoration-none text-white link-warning'><h5>Privacy Policy</h5></Link>
+                    
+                            <ul className="list-unstyled">
+                                <li></li>
+                            </ul> 
+                        </div>
                 </div>
             </div>
 
