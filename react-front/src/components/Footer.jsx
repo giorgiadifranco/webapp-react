bd16c81f030bf48781505c4b13b85dfb0d84c5cf
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-17 15:46:40 +0100

    fix footer

--- react-front/src/components/Footer.jsx
+++ react-front/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 export default function Footer(){
 
     return(
@@ -9,10 +9,31 @@ export default function Footer(){
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 col-lg-3 mb-3">
-                        <h5>Services</h5>
+                        <Link className='text-decoration-none text-white link-warning'><h5>About us</h5></Link>
                  
                         <ul className="list-unstyled">
-                            <li> list</li>
+                            <li></li>
+                        </ul> 
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-3">
+                    <Link className='text-decoration-none text-white link-warning'><h5>Services</h5></Link>
+                 
+                        <ul className="list-unstyled">
+                            <li></li>
+                        </ul> 
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-3">
+                    <Link className='text-decoration-none text-white link-warning'><h5>FAQ</h5></Link>
+                 
+                        <ul className="list-unstyled">
+                            <li></li>
+                        </ul> 
+                    </div>
+                    <div className="col-md-6 col-lg-3 mb-3">
+                    <Link className='text-decoration-none text-white link-warning'><h5>Privacy Policy</h5></Link>
+                 
+                        <ul className="list-unstyled">
+                            <li></li>
                         </ul> 
                     </div>
                 </div>
